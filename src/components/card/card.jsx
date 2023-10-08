7299ada8ecd654f3e8b3266ab1644754fe1f6bd9
carlosandf <carlos.andres.ff2912@gmail.com>
2023-10-08 09:36:33 -0500

    Change format lines in Card product

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,19 +2,16 @@ export const Card = ({ category, title, price, image }) => {
   return (
     <article className='bg-white cursor-pointer w-full h-64 rounded-xl overflow-hidden'>
       <figure className='relative mb-2 w-full h-4/5 overflow-hidden flex justify-center'>
-        <span
-          className='absolute bottom-2 left-2 bg-white/70 rounded-md text-black text-sm font-light px-3 py-0.5'
-        >
+        <span className='absolute bottom-2 left-2 bg-white/70 rounded-md text-black text-sm font-light px-3 py-0.5'>
           {category}
         </span>
         <img
           className='object-center object-cover h-full w-full'
           src={image}
           loading="lazy"
-          alt={title} />
-        <div
-          className='absolute top-2 right-2 grid place-content-center bg-gray-200 h-6 w-6 rounded-full'
-        >
+          alt={title}
+        />
+        <div className='absolute top-2 right-2 grid place-content-center bg-gray-200 h-6 w-6 rounded-full'>
           +
         </div>
       </figure>
