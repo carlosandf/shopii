cdc8fdd96c7d79b98ec9b219885ee8295b3ad264
carlosandf <carlos.andres.ff2912@gmail.com>
2023-10-08 23:33:44 -0500

    Update layout for use product detail aside

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,4 +1,6 @@
 import { Navbar } from '../navbar/navbar';
+import { ProductDetail } from '../product_detail/product_detail';
+import './layout.css';
 
 export const Layout = ({ children }) => {
   return (
@@ -6,8 +8,13 @@ export const Layout = ({ children }) => {
       <header className='fixed z-10 w-full top-0 max-w-screen-2xl'>
         <Navbar />
       </header>
-      <main className='mt-14 flex flex-col items-center p-8'>
-        {children}
+      <main className='mt-16 grid gap-1 max-h-[calc(100dvh-64px)] place-content-center grid-cols-[auto_auto_auto] items-center p-8'>
+        <div className='Layout-main col-[2/3] max-h-[inherit] overflow-y-scroll pt-3 rounded-s-lg'>
+          {children}
+        </div>
+        <div>
+          <ProductDetail />
+        </div>
       </main>
       <footer></footer>
     </>
