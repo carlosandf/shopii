e65728770ba1192d0b5778b9e32135ccf74af4f3
carlosandf <carlos.andres.ff2912@gmail.com>
2023-10-08 23:32:51 -0500

    Add product detail component

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export * from './nav_link/nav_link.jsx';
 export * from './navbar/navbar.jsx';
 export * from './layout/layout.jsx';
 export * from './card/card.jsx';
+export * from './product_detail/product_detail.jsx';
