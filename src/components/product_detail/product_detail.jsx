6744a402236ce1da2fb4a7424586c507db0744c8
carlosandf <carlos.andres.ff2912@gmail.com>
2023-10-08 23:30:27 -0500

    Create product detail component

---
+++ src/components/product_detail/product_detail.jsx
@@ -0,0 +1,7 @@
+export const ProductDetail = () => {
+  return (
+    <aside className="flex flex-col border bg-white border-black rounded-lg max-w-xs w-screen h-[calc(100dvh-64px)]">
+      ProductDetail
+    </aside>
+  );
+};
