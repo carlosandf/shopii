f40f2957363fe050a82ebd3f5973a82070f29246
carlosandf <carlos.andres.ff2912@gmail.com>
2023-11-18 23:42:30 -0500

    Change orders view

--- src/pages/my_orders/my_orders.jsx
+++ src/pages/my_orders/my_orders.jsx
@@ -1,6 +1,8 @@
-import { OrderCard } from '../../components/order_card';
+import { Link } from 'wouter';
+import { CartIcon } from '../../components/icons/index.jsx';
 import { useShoppingCartContext } from '../../hooks/useShoppingCartContext';
 import { dateFormat } from '../../utils/dateFormat.js';
+import { sumListNums } from '../../utils/sumListItems.js';
 
 export const MyOrders = () => {
   const { orders } = useShoppingCartContext();
@@ -9,21 +11,25 @@ export const MyOrders = () => {
       <header className="mb-8">
         <h3 className='text-2xl font-semibold'>My orders</h3>
       </header>
-      <div className='max-w-md'>
+      <div className=' w-[60vw]  grid gap-2'>
         {
           orders.map(order => (
-            <article className='grid gap-2 mb-10' key={order.date}>
-              <p className='italic'>{dateFormat(order.date)}</p>
-              {
-                order.products.map(({ image, title, id, price }) => (
-                  <OrderCard
-                    key={`order_${id}`}
-                    image={image}
-                    title={title}
-                    price={price}
-                  />
-                ))
-              }
+            <article key={order.date}>
+              <Link href={`/orders/${order.id}`}>
+                <div className='flex justify-between p-5 border border-gray-300 rounded-md cursor-pointer hover:shadow-md'>
+                  <section className='flex gap-4'>
+                    <CartIcon />
+                    <span>{order.products.length} products</span>
+                  </section>
+                  <section className='flex gap-4 font-bold'>
+                    <span>Total:</span>
+                    <span>$ {sumListNums(order.products.map(product => product.price))}</span>
+                  </section>
+                  <section>
+                    <p className='italic'>{dateFormat(order.date)}</p>
+                  </section>
+                </div>
+              </Link>
             </article>
           ))
         }
