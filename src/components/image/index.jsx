4135245a537518d8c8ee03e191b042cacf735ade
carlosandf <carlos.andres.ff2912@gmail.com>
2024-01-05 18:09:33 -0500

    Split logic: clean image component

--- src/components/image/index.jsx
+++ src/components/image/index.jsx
@@ -1,42 +1,7 @@
-import { useRef, useEffect, useState } from 'react';
-
-const defaultLoadImage = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=';
+import { useLazyLoading } from './useLazyLoading';
 
 export const Image = ({ src, onLazyLoad, ...imgProps }) => {
-  const node = useRef(null);
-  const [srcImage, setSrcImage] = useState(defaultLoadImage);
-  const [isLazyLoaded, setIsLazyLoaded] = useState(false);
-
-  useEffect(() => {
-    const loadImage = (img) => {
-      if (!img || !onLazyLoad) return;
-      img.onload = () => {
-        if (typeof onLazyLoad === 'function') onLazyLoad(img);
-        observer.disconnect();
-      };
-    };
-    // new observer
-    if (isLazyLoaded) return;
-
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (!entry.isIntersecting || !node.current) return;
-
-        setSrcImage(src);
-        loadImage(node.current);
-        setIsLazyLoaded(true);
-      });
-    });
-
-    // observe node
-    if (node.current) {
-      observer.observe(node.current);
-    }
-
-    return () => {
-      observer.disconnect();
-    };
-  }, [src, onLazyLoad, isLazyLoaded]);
+  const {node, srcImage} = useLazyLoading({src, onLazyLoad})
 
   return (
     <img
---
+++ src/components/image/useLazyLoading.js
@@ -0,0 +1,55 @@
+import { useRef, useEffect, useState } from 'react';
+
+const defaultLoadImage = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=';
+
+const loadImage = (img, callback = null) => {
+  if (!img && !callback) return;
+  img.onload = () => {
+    if (typeof callback === 'function') callback(img);
+  };
+  img.onerror = () => {
+    if (!navigator.onLine) {
+      const interval = setInterval(() => {
+        if (navigator.onLine) {
+          img.src = img.src;
+          clearInterval(interval);
+        }
+      }, 5000)
+    }
+  }
+};
+
+export const useLazyLoading = ({ src, onLazyLoad }) => {
+  const node = useRef(null);
+  const [srcImage, setSrcImage] = useState(defaultLoadImage);
+  const [isLazyLoaded, setIsLazyLoaded] = useState(false);
+
+  useEffect(() => {
+    // new observer
+    if (isLazyLoaded) return;
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting || !node.current) return;
+        loadImage(node.current, onLazyLoad);
+        setSrcImage(src);
+        setIsLazyLoaded(true);
+        observer.disconnect();
+      });
+    });
+
+    // observe node
+    if (node.current) {
+      observer.observe(node.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [src, onLazyLoad, isLazyLoaded]);
+
+  return {
+    srcImage,
+    node
+  };
+};
