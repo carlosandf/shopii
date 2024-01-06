bb0afa25a0764ce186232a2a6c0a24b99cdc1a46
carlosandf <carlos.andres.ff2912@gmail.com>
2024-01-05 19:26:29 -0500

    Add input for search

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,22 @@
-import { Card } from '../../components';
+import React from 'react';
+import { Card } from '../../components/index.js';
 import { useShoppingCartContext } from '../../hooks/useShoppingCartContext.js';
 
 export const Home = () => {
   const { products } = useShoppingCartContext();
 
+  const handleChange = (e) => {
+    console.log(e.target.value)
+  }
+
   return (
-    <>
+    <div className='grid gap-6'>
+      <input
+        className='m-auto w-full max-w-lg px-4 py-2 shadow rounded-lg outline-gray-300'
+        placeholder='Search a product'
+        type="text"
+        onChange={handleChange}
+      />
       <section
         id='home_product_container'
         className='grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-10 w-full justify-items-center'
@@ -24,6 +35,6 @@ export const Home = () => {
           />
         ))}
       </section>
-    </>
+    </div>
   );
 };
